2ab3fffc04e30471dbbf35b54ab59a4ff9f1b824
Mason <garrettm19@students.ecu.edu>
2023-09-10 21:51:27 -0400

    login, registration and task management pages created favicon also implemented

--- src/main/java/com/masongarrett/taskmanagementsystem/model/User.java
+++ src/main/java/com/masongarrett/taskmanagementsystem/model/User.java
@@ -1,25 +1,26 @@
 package com.masongarrett.taskmanagementsystem.model;
 
 import jakarta.persistence.*;
+import lombok.*;
+
 import java.sql.Date;
 
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity //@Entity is used to tell Java that this class represents a database table
 @Table(name = "user") //@Table is used to define the specific table
 public class User {
 
     @Id //@Id marks the primary key
     @GeneratedValue(strategy = GenerationType.IDENTITY) //@GeneratedValue(strategy = GenerationType.IDENTITY) is used to automatically generate values for identification
-    @Column(name = "id") //@Column defines the specific column
+    @Column(name = "user_id") //@Column defines the specific column
     private Long id;
-
-    @Column(name = "roleId", nullable = false)
-    private Short roleId;
 
     @Column(name = "firstName", length = 50)
     private String firstName;
-
-    @Column(name = "middleName", length = 50)
-    private String middleName;
 
     @Column(name = "lastName", length = 50)
     private String lastName;
@@ -33,141 +34,12 @@ public class User {
     @Column(name = "email", length = 50, unique = true)
     private String email;
 
-    @Column(name = "passwordHash", length = 32, nullable = false)
-    private String passwordHash;
+    @Column(name = "password", length = 32, nullable = false)
+    private String password;
 
     @Column(name = "registeredAt", nullable = false)
     private Date registeredAt;
 
-    @Column(name = "lastLogin")
-    private Date lastLogin;
-
-    @Column(name = "intro", columnDefinition = "TINYTEXT")
-    private String intro;
-
     @Column(name = "profile", columnDefinition = "TEXT")
     private String profile;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Short getRoleId() {
-        return roleId;
-    }
-
-    public void setRoleId(Short roleId) {
-        this.roleId = roleId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getMiddleName() {
-        return middleName;
-    }
-
-    public void setMiddleName(String middleName) {
-        this.middleName = middleName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getMobile() {
-        return mobile;
-    }
-
-    public void setMobile(String mobile) {
-        this.mobile = mobile;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPasswordHash() {
-        return passwordHash;
-    }
-
-    public void setPasswordHash(String passwordHash) {
-        this.passwordHash = passwordHash;
-    }
-
-    public Date getRegisteredAt() {
-        return registeredAt;
-    }
-
-    public void setRegisteredAt(Date registeredAt) {
-        this.registeredAt = registeredAt;
-    }
-
-    public Date getLastLogin() {
-        return lastLogin;
-    }
-
-    public void setLastLogin(Date lastLogin) {
-        this.lastLogin = lastLogin;
-    }
-
-    public String getIntro() {
-        return intro;
-    }
-
-    public void setIntro(String intro) {
-        this.intro = intro;
-    }
-
-    public String getProfile() {
-        return profile;
-    }
-
-    public void setProfile(String profile) {
-        this.profile = profile;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", roleId=" + roleId +
-                ", firstName='" + firstName + '\'' +
-                ", middleName='" + middleName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", username='" + username + '\'' +
-                ", mobile='" + mobile + '\'' +
-                ", email='" + email + '\'' +
-                ", passwordHash='" + passwordHash + '\'' +
-                ", registeredAt=" + registeredAt +
-                ", lastLogin=" + lastLogin +
-                ", intro='" + intro + '\'' +
-                ", profile='" + profile + '\'' +
-                '}';
-    }
 }
